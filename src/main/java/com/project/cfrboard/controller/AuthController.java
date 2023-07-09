a7f8666413f2fdc677c58f7c3e292f9eba3535ce
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-09 19:41:26 +0900

    구현 : AuthController join 회원가입

--- src/main/java/com/project/cfrboard/controller/AuthController.java
+++ src/main/java/com/project/cfrboard/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.project.cfrboard.controller;
 
+import com.project.cfrboard.domain.dto.MemberJoinDto;
 import com.project.cfrboard.domain.entity.Member;
+import com.project.cfrboard.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final MemberService memberService;
 
     @GetMapping("/loginForm")
     public String loginForm(@ModelAttribute Member member) {
@@ -22,5 +27,11 @@ public class AuthController {
     @GetMapping("/joinForm")
     public String joinForm(@ModelAttribute Member member) {
         return "auth/joinForm";
+    }
+
+    @PostMapping("/joinForm")
+    public String join(@ModelAttribute MemberJoinDto memberJoinDto) {
+        memberService.join(memberJoinDto);
+        return "redirect:/";
     }
 }
