670322d20b53c0b8a22102ad35dd3de11c5dd909
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-09 14:43:23 +0900

    Initial commit

---
+++ src/main/java/com/project/cfrboard/CfrboardApplication.java
@@ -0,0 +1,13 @@
+package com.project.cfrboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfrboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfrboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cfrboard/CfrboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cfrboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfrboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
