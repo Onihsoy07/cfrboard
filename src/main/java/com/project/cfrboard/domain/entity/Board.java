f492c375e4df6eab7ba55342721b81f505fe2fde
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-09 16:22:00 +0900

    Board 엔티티 추가

---
+++ src/main/java/com/project/cfrboard/domain/entity/Board.java
@@ -0,0 +1,26 @@
+package com.project.cfrboard.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(schema = "cfrboard")
+public class Board extends Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false, unique = false)
+    private String title;
+
+    @Column(nullable = false, unique = false)
+    @Lob
+    private String content;
+
+}
