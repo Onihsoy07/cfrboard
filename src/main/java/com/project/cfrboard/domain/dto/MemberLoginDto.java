c00e939b1a86ee0e0a141eac3ba5b9238c767075
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-14 20:20:52 +0900

    구현 : MemberLoginDto

--- src/main/java/com/project/cfrboard/domain/dto/MemberLoginDto.java
+++ src/main/java/com/project/cfrboard/domain/dto/MemberLoginDto.java
@@ -1,13 +1,16 @@
 package com.project.cfrboard.domain.dto;
 
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
-public class MemberJoinDto {
+public class MemberLoginDto {
 
+    @NotBlank
     private String username;
 
+    @NotBlank
     private String password;
+
 }
