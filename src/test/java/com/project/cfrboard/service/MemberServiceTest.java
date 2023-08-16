19895684aea1016ca838996bb4d725c3fe826e0b
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-16 23:12:26 +0900

    세팅 : ModelMapper 빈 등록

--- src/test/java/com/project/cfrboard/service/MemberServiceTest.java
+++ src/test/java/com/project/cfrboard/service/MemberServiceTest.java
@@ -1,18 +1,12 @@
 package com.project.cfrboard.service;
 
 import com.project.cfrboard.domain.dto.MemberJoinDto;
-import com.project.cfrboard.domain.entity.Member;
-import com.project.cfrboard.domain.entity.Role;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
