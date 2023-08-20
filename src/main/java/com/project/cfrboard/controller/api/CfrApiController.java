8e9a60e933ebf716c7370b06869c20d27c258b73
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-20 21:34:04 +0900

    수정 : CFR 저장 컨트롤러 API로 변경

--- src/main/java/com/project/cfrboard/controller/api/CfrApiController.java
+++ src/main/java/com/project/cfrboard/controller/api/CfrApiController.java
@@ -1,7 +1,6 @@
-package com.project.cfrboard.controller;
+package com.project.cfrboard.controller.api;
 
 import com.project.cfrboard.auth.PrincipalDetails;
-import com.project.cfrboard.domain.dto.CfrDataDto;
 import com.project.cfrboard.domain.dto.CfrResponseDto;
 import com.project.cfrboard.exception.NotOnePeoplePhotoException;
 import com.project.cfrboard.service.CfrService;
@@ -9,39 +8,32 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/cfr")
-public class CfrController {
+public class CfrApiController {
 
     private final CfrService cfrService;
 
-    @GetMapping
-    public String cfrForm() {
-        return "cfr/cfrForm";
-    }
-
-    @GetMapping("/list")
-    public String list(@AuthenticationPrincipal PrincipalDetails principal,
-                       Model model) {
-        if (principal == null) {
-            throw new AccessDeniedException("권한없음");
+    @PostMapping
+    public ResponseEntity<Boolean> saveCfrData(@RequestParam("image") MultipartFile multipartFile,
+                                      @AuthenticationPrincipal PrincipalDetails principal) {
+        CfrResponseDto cfrResponseDto = cfrService.getCfrResponseDto(multipartFile);
+        try {
+            cfrService.save(cfrResponseDto, principal.getMember());
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+        } catch (NotOnePeoplePhotoException e) {
+            log.error("사진의 사람의 수가 0 or 다수입니다.", e);
+            return ResponseEntity.status(HttpStatus.OK).body(false);
         }
-        model.addAttribute("cfrDataList", cfrService.getCfrList(principal.getMember()));
-        return "cfr/list";
     }
 
 }
