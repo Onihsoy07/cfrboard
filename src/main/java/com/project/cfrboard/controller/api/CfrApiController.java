aabcbd5f9102c17975b3664ee30ec1a81100a937
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-21 20:49:30 +0900

    프론트 수정 : CFR API 전송 프론트, JS 수정

--- src/main/java/com/project/cfrboard/controller/api/CfrApiController.java
+++ src/main/java/com/project/cfrboard/controller/api/CfrApiController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -25,7 +22,7 @@ public class CfrApiController {
 
     @PostMapping
     public ResponseEntity<Boolean> saveCfrData(@RequestParam("image") MultipartFile multipartFile,
-                                      @AuthenticationPrincipal PrincipalDetails principal) {
+                                               @AuthenticationPrincipal PrincipalDetails principal) {
         CfrResponseDto cfrResponseDto = cfrService.getCfrResponseDto(multipartFile);
         try {
             cfrService.save(cfrResponseDto, principal.getMember());
--- src/main/resources/static/js/cfr.js
+++ src/main/resources/static/js/cfr.js
@@ -1,3 +1,30 @@
+$(function() {
+    $("#upload_btn").click(function() {
+//        $("#cfr_form").preventDefault(); // 폼의 자체 서브밋 동작을 비활성
+
+        var imageInput = $("#imageFile")[0];
+        var formData = new FormData();
+        formData.append("image", imageInput.files[0]);
+
+        $.ajax({
+            url : "/cfr",
+            type : "POST",
+            data : formData,
+            processData: false, //프로세스 데이터 설정 : false 값을 해야 form data로 인식합니다
+            contentType: false //헤더의 Content-Type을 설정 : false 값을 해야 form data로 인식합니다
+        }).done(function(res){
+            if(res == true) {
+                location.href = "/cfr/list";
+            } else {
+                alert("사진이 조건에 맞지 않습니다.");
+            }
+        }).fail(function(error){
+            alert("통신에 실패하였습니다.");
+        });
+    });
+
+});
+
 function inputFile() {
     $('#imageFile').click();
 }
