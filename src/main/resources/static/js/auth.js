ad56f4dfc35fdeba9dc1b81e178d5d06ac573c37
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-27 22:24:53 +0900

    프론트 수정 : 비밀번호 확인 form submit에서 api로 수정

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -47,12 +47,27 @@ $(function() {
     $("#password_check").click(function() {
         let password = $("#password").val();
 
-        console.log(password);
-
         if(password == "") {
             alert("비밀번호를 입력하세요.");
         } else {
-            $("form").submit();
+            var formData = new FormData();
+            formData.append("password", password);
+
+            $.ajax({
+                url : "/auth/confirm",
+                type : "POST",
+                data : formData,
+                processData: false,
+                contentType: false
+            }).done(function(res){
+                if(res == true) {
+                    location.href = "/member";
+                } else {
+                    alert("비밀번호가 다릅니다.");
+                }
+            }).fail(function(error){
+                alert("통신에 실패하였습니다.");
+            });
         }
     });
 
