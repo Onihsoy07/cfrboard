f2c75deac64778136ae661a603b33261e1a996ec
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-08-28 22:52:00 +0900

    프론트 구현 : 회원정보 수정

--- src/main/java/com/project/cfrboard/controller/api/MemberApiController.java
+++ src/main/java/com/project/cfrboard/controller/api/MemberApiController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -25,4 +25,42 @@ $(function() {
             });
         }
     });
+
+    $("#btn-modify").click(function() {
+        let data = {
+            password : $("#password").val(),
+            passwordCheck : $("#passwordCheck").val()
+        };
+
+        if(data.password == "") {
+            alert("비밀번호를 입력하세요.");
+        } else if(data.passwordCheck == "") {
+            alert("비밀번호 확인을 입력하세요.");
+        } else if(data.password != data.passwordCheck) {
+            alert("비밀번호와 비밀번호 확인이 다릅니다.");
+        } else {
+            $.ajax({
+                url : "/member",
+                type : "PUT",
+                contentType: "application/json;charset=utf-8",
+                dataType:"json",
+                data : JSON.stringify(data)
+            }).done(function(res) {
+                if(res == "ok") {
+                    alert("비밀번호 수정이 완료되었습니다.");
+                    location.href = "/";
+                } else if(res = "same") {
+                    alert("기존 비밀번호와 새 비밀번호가 일치합니다.");
+                    return false;
+                } else if(res = "non") {
+                    alert("비밀번호와 비밀번호 확인이 다릅니다.");
+                    return false;
+                }
+            }).fail(function(err) {
+                alert("통신에 실패하였습니다.");
+            });
+        }
+    });
+
+
 });
