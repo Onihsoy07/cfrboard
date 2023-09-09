23ab42c9f9310c7fa6ed09c36d464544b3fc797e
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-09-09 10:17:50 +0900

    세팅 수정 : postman api 테스트 위한 인증방식

--- src/main/java/com/project/cfrboard/config/WebSecurityConfig.java
+++ src/main/java/com/project/cfrboard/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -44,6 +45,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                //postman api 테스트 시 authorization 방식
+                .httpBasic()
+                .and()
                 .authorizeRequests()
                     .anyRequest().permitAll()
                 .and()
