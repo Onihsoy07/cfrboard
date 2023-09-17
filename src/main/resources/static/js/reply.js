b3328d07d0d69da1650ae19de6c166a98c7270d4
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-09-17 23:46:16 +0900

    프론트 구현 : 댓글 작성

---
+++ src/main/resources/static/js/reply.js
@@ -0,0 +1,32 @@
+function writeReply(boardId, replyId, depth) {
+        let data = {
+            comment : $("#comment").val(),
+            boardId : boardId,
+            replyId : replyId,
+            depth : depth
+        }
+
+        if(data.comment.length < 3) {
+            alert("댓글을 3자 이상 써주세요.")
+            return false;
+        }
+
+        $.ajax({
+            url : "/replys",
+            type : "POST",
+            contentType: "application/json;charset=utf-8",
+            dataType:"json",
+            data : JSON.stringify(data)
+        }).done(function (res) {
+            console.log(res);
+            if(res.httpsCode != 200) {
+                alert("댓글 쓰기가 실패되었습니다.");
+            } else {
+                alert("댓글 쓰기가 완료되었습니다.");
+//                $('#replyWin').load(location.href + ' #replyBox');
+            }
+        }).fail(function (error){
+            console.log(error);
+            alert("댓글 쓰기가 실패되었습니다.");
+        });
+}
