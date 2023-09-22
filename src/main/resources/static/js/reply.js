7dc65ac32bc0b3765358c5bc4cbc734d73fd108e
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-09-22 21:46:46 +0900

    프론트 구현 : 대댓글 작성

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -20,8 +20,7 @@ function writeReply(boardId, replyId, depth) {
         }).done(function (res) {
             if(res.success) {
                 alert("댓글 쓰기가 완료되었습니다.");
-//                $('#replyWin').load(location.href + ' #replyBox');
-
+                $(".reply-wrapper").load(location.href + " .comment-outer, .reply-outer");
             } else {
                 alert("댓글 쓰기가 실패되었습니다.");
             }
@@ -29,4 +28,28 @@ function writeReply(boardId, replyId, depth) {
             console.log(error);
             alert("댓글 쓰기가 실패되었습니다.");
         });
+}
+
+function replyOpen(boardId, replyId, depth) {
+    $("div").remove(".reReply-inner");
+    $(".reply-inner").css("display", "none");
+
+    let username = $("#p_username").val();
+
+    let html = "";
+
+    html += "<div class='reReply-inner'>";
+    html += "<div class='reply-info'>";
+    html += "<div class='member-info'>" + username +  "</div>";
+    html += "</div>";
+    html += "<div class='reply-textarea-wrapper'>";
+    html += "<textarea class='reply-textarea' id='comment' maxlength='800' required='required' style='height: 99.5px;'></textarea>";
+    html += "<div class='reply-button-wrapper'>";
+    html += "<button class='reply-button' type='button' onclick='writeReply(" + boardId + ", " + replyId + ", " + depth + ")'>작성</button>";
+    html += "</div>";
+    html += "</div>";
+    html += "</div>";
+
+    $("#c_" + replyId).after(html);
+
 }
