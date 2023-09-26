c85dc278f6966475d1c58d46560d3a0c1cb6d9c7
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-09-26 23:16:54 +0900

    구현 : 메인 페이지 컨트롤러

--- src/main/java/com/project/cfrboard/controller/IndexController.java
+++ src/main/java/com/project/cfrboard/controller/IndexController.java
@@ -1,15 +1,30 @@
 package com.project.cfrboard.controller;
 
+import com.project.cfrboard.domain.entity.enumeration.BoardTable;
+import com.project.cfrboard.service.BoardService;
+import com.project.cfrboard.service.CfrService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final BoardService boardService;
+    private final CfrService cfrService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("currentFreeBoard", boardService.getCurrentBoard(BoardTable.FREE));
+        model.addAttribute("currentCfrBoard", boardService.getCurrentBoard(BoardTable.CFR));
+        model.addAttribute("topTodayView", boardService.getTodayTopView());
+        model.addAttribute("topCfr", cfrService.getTopConfidenceCfrData());
+
         return "index";
     }
 }
