cec5fadcfb60b3080a8d94dff7ae7cc8c5b6b35c
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-10-05 22:31:12 +0900

    수정 : JVM TimeZone UTC 변경

--- src/main/java/com/project/cfrboard/CfrboardApplication.java
+++ src/main/java/com/project/cfrboard/CfrboardApplication.java
@@ -3,11 +3,19 @@ package com.project.cfrboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CfrboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfrboardApplication.class, args);
+	}
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
