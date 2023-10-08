302817c41fd7be1fe77bc3616fdbfbe8c4ece92e
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-10-08 23:35:51 +0900

    수정 : 총 페이지 수 일의 자리 1로 끝날 때 마지막 오프셋 페이지 11페이지 보이는 오류 수정

--- src/main/java/com/project/cfrboard/service/PageService.java
+++ src/main/java/com/project/cfrboard/service/PageService.java
@@ -1,14 +1,12 @@
 package com.project.cfrboard.service;
 
 import com.project.cfrboard.domain.dto.PageDto;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -20,11 +18,11 @@ public class PageService {
 
     public PageDto getPageOffset(Pageable pageable, Page<?> pageList) {
         int startPage = ((pageable.getPageNumber()-1)/10)*10;
-        int endPage = pageList.getTotalPages()<=10 ? pageList.getTotalPages() : (startPage + 11);
+        int endPage = pageList.getTotalPages()<=(startPage + 11) ? pageList.getTotalPages() : (startPage + 11);
         Boolean isPreviousPage = startPage==0 ? false : true;
         Boolean isNextPage = true;
 
-        if (pageList.getTotalPages() <= endPage) {
+        if ((endPage-startPage) <= 10) {
             endPage = pageList.getTotalPages()+1;
             isNextPage = false;
         }
