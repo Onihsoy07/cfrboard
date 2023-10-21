339b002c22ea91750063a8a166b6832b639b8b33
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-10-21 23:12:48 +0900

    수정 : 비밀번호 변경 DTO 패턴 추가

--- src/main/java/com/project/cfrboard/domain/dto/MemberUpdateDto.java
+++ src/main/java/com/project/cfrboard/domain/dto/MemberUpdateDto.java
@@ -5,14 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberUpdateDto {
 
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{0,999}$", message="비밀번호는 영어와 숫자의 조합을 사용해주세요.")
+    @Size(min = 4, max = 20, message="비밀번호는 4~20자리로 해주세요.")
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+
     @NotBlank(message = "비밀번호 확인을 입력해주세요.")
     private String passwordCheck;
 }
