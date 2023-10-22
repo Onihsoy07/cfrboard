57d7be0a93ad4c3c6fb650534455b5f47a1b1805
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-10-22 23:33:10 +0900

    수정 : 회원가입 아이디, 비밀번호 확인 정규식 수정

--- src/main/java/com/project/cfrboard/domain/dto/MemberJoinDto.java
+++ src/main/java/com/project/cfrboard/domain/dto/MemberJoinDto.java
@@ -11,12 +11,12 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 public class MemberJoinDto {
 
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{0,999}$", message="아이디는 영어와 숫자의 조합을 사용해주세요.")
+    @Pattern(regexp = "^[a-z0-9]+$", message="아이디는 소문자 영어와 숫자의 조합을 사용해주세요.")
     @Size(min = 4, max = 10, message="아이디는 4~10자리로 해주세요.")
     @NotBlank(message = "아이디를 입력해주세요.")
     private String username;
 
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{0,999}$", message="비밀번호는 영어와 숫자의 조합을 사용해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message="비밀번호는 영어와 숫자의 조합을 사용해주세요.")
     @Size(min = 4, max = 20, message="비밀번호는 4~20자리로 해주세요.")
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
