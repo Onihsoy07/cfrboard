3a4783d81abaf520c6296f03e755fadb15500d11
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-10-23 23:19:38 +0900

    수정 : 아이디, 비밀번호 규칙 정규식 수정

--- src/main/java/com/project/cfrboard/domain/dto/MemberUsernameDuplicationCheckDto.java
+++ src/main/java/com/project/cfrboard/domain/dto/MemberUsernameDuplicationCheckDto.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 public class MemberUsernameDuplicationCheckDto {
 
-    @Pattern(regexp = "^[a-z0-9]+$", message="아이디는 영어와 숫자의 조합을 사용해주세요.")
+    @Pattern(regexp = "^[a-z]+[0-9]+[a-z0-9]*$", message="아이디는 소문자 영어와 숫자의 조합을 사용해주세요.")
     @Size(min = 4, max = 10, message="아이디는 4~10자리로 해주세요.")
     private String username;
 }
