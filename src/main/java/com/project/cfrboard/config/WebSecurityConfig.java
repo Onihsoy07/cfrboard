2a5a6bd941862b4410a39d7c88ee6215608be049
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-10-24 23:01:13 +0900

    수정 : security 블랙리스트, 로그인 failerUrl 추가

--- src/main/java/com/project/cfrboard/config/WebSecurityConfig.java
+++ src/main/java/com/project/cfrboard/config/WebSecurityConfig.java
@@ -7,13 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -53,16 +53,20 @@ public class WebSecurityConfig {
                     .antMatchers("/likes/**").authenticated()
                     .antMatchers("/members/**").authenticated()
                     .antMatchers("/boards").authenticated()
+                    .antMatchers(HttpMethod.PUT, "/boards/**").authenticated()
+                    .antMatchers(HttpMethod.DELETE, "/boards/**").authenticated()
+                    .antMatchers("/boards/*/update-form").authenticated()
+                    .antMatchers("/inquirys/form").authenticated()
                     .antMatchers("/replys/**").authenticated()
                     .anyRequest().permitAll()
                 .and()
                 .formLogin()
                     .loginPage("/auth/login")
                     .loginProcessingUrl("/loginProc")
-                    .failureUrl("/auth/login")
+                    .failureUrl("/auth/login?result=fail")
                     .usernameParameter("username")
                     .passwordParameter("password")
-                    .defaultSuccessUrl("/")
+//                    .defaultSuccessUrl("/")
                     .successHandler(new CustomAuthSuccessHandler())
                 .and()
                 .logout()
