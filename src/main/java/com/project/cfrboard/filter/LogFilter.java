d583463bdb40967dd8ab97a1beb4b1e142f25b13
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-03 23:14:22 +0900

    수정 : 로그에 클라이언트 IP 추가

--- src/main/java/com/project/cfrboard/filter/LogFilter.java
+++ src/main/java/com/project/cfrboard/filter/LogFilter.java
@@ -2,10 +2,13 @@ package com.project.cfrboard.filter;
 
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class LogFilter implements Filter {
@@ -16,7 +19,41 @@ public class LogFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         MDC.put("requestURL", httpServletRequest.getRequestURL().toString());
 
+//        MDC.put("clientIP", (null != httpServletRequest.getHeader("X-FORWARDED-FOR")) ? httpServletRequest.getHeader("X-FORWARDED-FOR") : request.getRemoteAddr());
+        MDC.put("clientIP", getClientIp(httpServletRequest));
+
         chain.doFilter(httpServletRequest, response);
         MDC.clear();
     }
+    private String getClientIp(HttpServletRequest request) {
+        String clientIp = null;
+        boolean isIpInHeader = false;
+
+        List<String> headerList = new ArrayList<>();
+        headerList.add("X-Forwarded-For");
+        headerList.add("HTTP_CLIENT_IP");
+        headerList.add("HTTP_X_FORWARDED_FOR");
+        headerList.add("HTTP_X_FORWARDED");
+        headerList.add("HTTP_FORWARDED_FOR");
+        headerList.add("HTTP_FORWARDED");
+        headerList.add("Proxy-Client-IP");
+        headerList.add("WL-Proxy-Client-IP");
+        headerList.add("HTTP_VIA");
+        headerList.add("IPV6_ADR");
+
+        for (String header : headerList) {
+            clientIp = request.getHeader(header);
+            if (StringUtils.hasText(clientIp) && !clientIp.equals("unknown")) {
+                isIpInHeader = true;
+                break;
+            }
+        }
+
+        if (!isIpInHeader) {
+            clientIp = request.getRemoteAddr();
+        }
+
+        return clientIp;
+    }
+
 }
